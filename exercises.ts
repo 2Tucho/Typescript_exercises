a0056e86b7babe8519fe5ce3394c8c5c8b18a6f3
2Tucho <albergromero@gmail.com>
2024-09-04 19:36:06 +0200

    Exercises_1_and_2

---
+++ exercises.ts
@@ -0,0 +1,16 @@
+/* 1 */
+let nombrePila:string = "Miguelo";
+let age:number = 30;
+const CHARACTER:{nombre:string, edad:number} = {
+    nombre: nombrePila,
+    edad: age
+};
+console.log(CHARACTER)
+
+/* 2 */
+interface Superhero {
+    nombre:string,
+    poderes:string[]
+}
+
+/* 3 */
--- questions.js
+++ questions.js
@@ -1,4 +1,4 @@
-/* Este código está hecho en JavaScript y hay que pasarlo a TypeScript empleando variables “let” y constantes. */
+/* (1) Este código está hecho en JavaScript y hay que pasarlo a TypeScript empleando variables “let” y constantes. */
 var nombre = "Miguelo";
 var edad= 30;
 var PERSONAJE = {
@@ -6,19 +6,19 @@ var PERSONAJE = {
     edad: edad
 }; 
 
-/* Crear una interface en TypeScript a partir de este código JavaScript: */
+/* (2) Crear una interface en TypeScript a partir de este código JavaScript: */
 var spiderman = {
     nombre: "Peter parket",
     poderes: ["trepar", "fuerza", "agilidad", "telas de araña"]
 };
 
-/* Ahora en TypeScript vamos a crear la clase Rombo, la cual debe tener dos propiedades:
+/* (3) Ahora en TypeScript vamos a crear la clase Rombo, la cual debe tener dos propiedades:
 DiagonalVertical y DiagonalHorizontal.
 Le añadiremos un constructor al que le pasaremos los valores anteriores cuando instanciemos el objeto.
 Y también debe de tener un método que calcule el area, que será la multiplicación de DiagonalVertical * DiagonalHorizontal.
 Este método devolverá un número. */
-
-/*En este ejercicio vamos a cambiar una función normal de javascript a una función de tipo flecha.
+ 
+/* (4) En este ejercicio vamos a cambiar una función normal de javascript a una función de tipo flecha.
 Cómo ya sabrás las funciones de flecha no alteran el “this” cuando se corren, por lo que son interesantes para cubrir algunos problemas de JavaScript.*/
 function suma( a, b ){
   return (a + b) 
